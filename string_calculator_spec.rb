09960052923fb7f9d302e14b89e24e93b1a1e26c
Prakhar Gupta <pgupta@plansource.com>
2025-07-29 12:37:25 +0530

    Adding a spec file

---
+++ string_calculator_spec.rb
@@ -0,0 +1,13 @@
+require "./string_calculator"
+
+describe "Add all the numbers in a string" do
+  it "returns 0 for an empty string" do
+  end
+
+  it "return sum with only one number in string" do
+  end
+
+  it "return sum with more than one number in string" do
+  end
+
+end
