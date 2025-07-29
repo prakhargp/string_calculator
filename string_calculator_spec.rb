8f0e70ab0016f0954c73386b68be65785b2f8c73
Prakhar Gupta <pgupta@plansource.com>
2025-07-29 18:07:43 +0530

    Refactored the specs

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -2,46 +2,40 @@ require "./string_calculator"
 
 describe "Add all the numbers in a string" do
 
+  subject { StringCalculator.new }
+
   it "returns 0 for an empty string" do
-    calc = StringCalculator.new
-    expect(calc.add("")).to eq(0)
+    expect(subject.add("")).to eq(0)
   end
 
   it "return sum with only one number in string" do
-    calc = StringCalculator.new
-    expect(calc.add("1")).to eq(1)
+    expect(subject.add("1")).to eq(1)
   end
 
   it "return sum with more than one number in string" do
-    calc = StringCalculator.new
-    expect(calc.add("1, 2")).to eq(3)
+    expect(subject.add("1, 2")).to eq(3)
   end
 
   it "return sum with more than one number in string separated by newline" do
-    calc = StringCalculator.new
-    expect(calc.add("1\n2, 3")).to eq(6)
+    expect(subject.add("1\n2, 3")).to eq(6)
   end
 
   it "should raise an exception for negative numbers" do
-    calc = StringCalculator.new
-    expect { calc.add("1\n-2,3") }.to raise_error("negative numbers not allowed (-2)")
+    expect { subject.add("1\n-2,3") }.to raise_error("negative numbers not allowed (-2)")
   end
 
   it "should raise an exception for negative numbers with numbers separated with commas" do
-    calc = StringCalculator.new
-    expect { calc.add("1\n-2,3, -4, -5\n6,-7") }.to raise_error("negative numbers not allowed (-2, -4, -5, -7)")
+    expect { subject.add("1\n-2,3, -4, -5\n6,-7") }.to raise_error("negative numbers not allowed (-2, -4, -5, -7)")
   end
 
   it "should raise exception for negative numbers with given delimiter" do
-    calc = StringCalculator.new
-    expect { calc.add("//;:\n1;:-2;:3;: -4;: -5;:6;:-7") }.to raise_error("negative numbers not allowed (-2, -4, -5, -7)")
-    expect { calc.add("///\n1/-2/3/ -4/ -5/6/-7") }.to raise_error("negative numbers not allowed (-2, -4, -5, -7)")
+    expect { subject.add("//;:\n1;:-2;:3;: -4;: -5;:6;:-7") }.to raise_error("negative numbers not allowed (-2, -4, -5, -7)")
+    expect { subject.add("///\n1/-2/3/ -4/ -5/6/-7") }.to raise_error("negative numbers not allowed (-2, -4, -5, -7)")
   end
 
   it "should return sum with given delimiter" do
-    calc = StringCalculator.new
-    expect(calc.add("//;\n1; 1; 1")).to eq(3)
-    expect(calc.add("//**\n2** 3** 1")).to eq(6)
+    expect(subject.add("//;\n1; 1; 1")).to eq(3)
+    expect(subject.add("//**\n2** 3** 1")).to eq(6)
   end
 
 end
