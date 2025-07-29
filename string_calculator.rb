4aa81350ac72924c6637fffa32b74324de466477
Prakhar Gupta <pgupta@plansource.com>
2025-07-29 14:48:47 +0530

    Added logic to handle negative numbers with exception message with numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,12 +5,17 @@ class StringCalculator
     # Splitting by commas or newlines
     num_arr = numbers.split(/,|\n/)
 
-    # Convert into intergers and check for negative number and if not adds up
+    # Convert into integers and check for negative number and if not adds up
+    negative_numbers = []
     num_arr.each do |num|
-      if num.to_i < 0
-        raise "negative numbers not allowed #{num.to_i}"
-      end
+      num = num.to_i
+      negative_numbers << num if num < 0
       sum += num.to_i
+    end
+
+    # If there are negative numbers, raise an exception
+    unless negative_numbers.empty?
+      raise "negative numbers not allowed (#{negative_numbers.join(', ')})"
     end
 
     sum
