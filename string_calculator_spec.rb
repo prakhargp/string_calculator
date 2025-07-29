8c6b8a3e11875a3d329ef4121e3ab69f938d85bd
Prakhar Gupta <pgupta@plansource.com>
2025-07-29 14:27:17 +0530

    Added spec for string with newline and comma

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -16,4 +16,9 @@ describe "Add all the numbers in a string" do
     expect(calc.add("1, 2")).to eq(3)
   end
 
+  it "return sum with more than one number in string separated by newline" do
+    calc = StringCalculator.new
+    expect(calc.add("1\n2, 3")).to eq(6)
+  end
+
 end
