05525dc5c01ef0bbf1c060762afa50ce5408bf23
Prakhar Gupta <pgupta@plansource.com>
2025-07-29 18:05:10 +0530

    Added spec for delimiter scenario

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -1,6 +1,7 @@
 require "./string_calculator"
 
 describe "Add all the numbers in a string" do
+
   it "returns 0 for an empty string" do
     calc = StringCalculator.new
     expect(calc.add("")).to eq(0)
@@ -29,6 +30,18 @@ describe "Add all the numbers in a string" do
   it "should raise an exception for negative numbers with numbers separated with commas" do
     calc = StringCalculator.new
     expect { calc.add("1\n-2,3, -4, -5\n6,-7") }.to raise_error("negative numbers not allowed (-2, -4, -5, -7)")
+  end
+
+  it "should raise exception for negative numbers with given delimiter" do
+    calc = StringCalculator.new
+    expect { calc.add("//;:\n1;:-2;:3;: -4;: -5;:6;:-7") }.to raise_error("negative numbers not allowed (-2, -4, -5, -7)")
+    expect { calc.add("///\n1/-2/3/ -4/ -5/6/-7") }.to raise_error("negative numbers not allowed (-2, -4, -5, -7)")
+  end
+
+  it "should return sum with given delimiter" do
+    calc = StringCalculator.new
+    expect(calc.add("//;\n1; 1; 1")).to eq(3)
+    expect(calc.add("//**\n2** 3** 1")).to eq(6)
   end
 
 end
