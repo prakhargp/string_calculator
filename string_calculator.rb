57bd33f1511cf30a6105b11ad67376f0af6513db
Prakhar Gupta <pgupta@plansource.com>
2025-07-29 14:21:11 +0530

    Added logic for sum with delimiter as comma

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,12 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+    sum = 0
+    # Splitting by commas
+    num_arr = numbers.split(',')
+
+    # Convert to integers and add up
+    num_arr.each { |num| sum += num.to_i }
+    sum
   end
 end
