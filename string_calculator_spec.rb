5d497dd586a432fc7c09351d88de2ea35d5b699c
Prakhar Gupta <pgupta@plansource.com>
2025-07-29 14:15:45 +0530

    Added first test case for empty string

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -2,6 +2,8 @@ require "./string_calculator"
 
 describe "Add all the numbers in a string" do
   it "returns 0 for an empty string" do
+    calc = StringCalculator.new
+    expect(calc.add("")).to eq(0)
   end
 
   it "return sum with only one number in string" do
